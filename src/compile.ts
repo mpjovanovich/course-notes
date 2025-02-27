528553c53127a42c21ed2cd90ecdb72741836630
Jonny Danger <mpjovanovich@gmail.com>
2025-02-26 21:01:20 -0500

    Pre-deploy commit

--- src/compile.ts
+++ src/compile.ts
@@ -122,44 +122,6 @@ async function processDirectory(
   markdownDirectory: string,
   outputFileDepth: number
 ): Promise<void> {
-  // Copy Bootstrap JS
-  const bootstrapSourceFile = path.join(
-    config.paths.bootstrapJsDir,
-    config.paths.bootstrapJsFile
-  );
-  const bootstrapDestFile = path.join(
-    config.paths.output,
-    config.paths.assets,
-    "scripts",
-    config.paths.bootstrapJsFile
-  );
-  await fs.copy(bootstrapSourceFile, bootstrapDestFile);
-
-  // Copy Bootstrap Icons CSS
-  const bootstrapIconsSource = path.join(
-    config.paths.bootstrapFontsDir,
-    "bootstrap-icons.min.css"
-  );
-  const bootstrapIconsDest = path.join(
-    config.paths.output,
-    config.paths.assets,
-    "css",
-    "bootstrap-icons.css"
-  );
-  await fs.copy(bootstrapIconsSource, bootstrapIconsDest);
-
-  // Copy Bootstrap Icons fonts directory
-  const bootstrapIconsFontSource = path.join(
-    config.paths.bootstrapFontsDir,
-    "fonts"
-  );
-  const bootstrapIconsFontDest = path.join(
-    config.paths.output,
-    config.paths.assets,
-    "css/fonts"
-  );
-  await fs.copy(bootstrapIconsFontSource, bootstrapIconsFontDest);
-
   const entries = await fs.readdir(markdownDirectory, { withFileTypes: true });
 
   for (const entry of entries) {
@@ -212,6 +174,44 @@ async function processDirectory(
 /* **********************************
  * Main
  * ********************************** */
+
+// Copy Bootstrap JS
+const bootstrapSourceFile = path.join(
+  config.paths.bootstrapJsDir,
+  config.paths.bootstrapJsFile
+);
+const bootstrapDestFile = path.join(
+  config.paths.output,
+  config.paths.assets,
+  "scripts",
+  config.paths.bootstrapJsFile
+);
+await fs.copy(bootstrapSourceFile, bootstrapDestFile);
+
+// Copy Bootstrap Icons CSS
+const bootstrapIconsSource = path.join(
+  config.paths.bootstrapFontsDir,
+  "bootstrap-icons.min.css"
+);
+const bootstrapIconsDest = path.join(
+  config.paths.output,
+  config.paths.assets,
+  "css",
+  "bootstrap-icons.css"
+);
+await fs.copy(bootstrapIconsSource, bootstrapIconsDest);
+
+// Copy Bootstrap Icons fonts directory
+const bootstrapIconsFontSource = path.join(
+  config.paths.bootstrapFontsDir,
+  "fonts"
+);
+const bootstrapIconsFontDest = path.join(
+  config.paths.output,
+  config.paths.assets,
+  "css/fonts"
+);
+await fs.copy(bootstrapIconsFontSource, bootstrapIconsFontDest);
 
 // Add static assets to the output directory
 await fs.copy(config.paths.assets, config.paths.output + "assets");
